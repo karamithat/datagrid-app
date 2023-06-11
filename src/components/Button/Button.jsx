4c02f72a2ef20ee58358d0cc0eff60f6aca7d41b
karamithat <mithatkara93@gmail.com>
2023-06-11 22:21:33 +0300

    add new components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,73 @@
-import "./Button.css"
+import { useState } from "react";
+import Modal from "react-modal";
+import "./Button.css";
+import PropTypes from 'prop-types';
 
-const Button = () => {
+
+Modal.setAppElement("#root");
+
+const Button = ({ setData }) => {
+  const [link, setLink] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function handleSave() {
+    setData((prevData) => [
+      ...prevData,
+      { link: link, name: name, description: description },
+    ]);
+    closeModal();
+    setLink("");
+    setName("");
+    setDescription("");
+  }
+
   return (
     <div>
-
-    <button className="button">
-    <span>Yeni Hesap Ekle</span>
-  </button>
+      <button className="button" onClick={openModal}>
+        <span>Yeni Hesap Ekle</span>
+      </button>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="modal">
+        <button className="close" onClick={closeModal}>
+          X
+        </button>
+        <span>Sosyal Medya Linki</span>
+        <input
+          type="text"
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+        />
+        <span>Sosyal Medya Adı</span>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <span>Açıklama</span>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <div className="modal-buttons">
+          <button onClick={closeModal}>Vazgeç</button>
+          <button onClick={handleSave}>Kaydet</button>
+        </div>
+      </Modal>
     </div>
-  )
-}
-
-export default Button
+  );
+};
+Button.propTypes = {
+  setData: PropTypes.func.isRequired,
+};
+export default Button;
--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,33 +1,74 @@
-import DataGrid, { Column } from "devextreme-react/data-grid";
+import DataGrid, { Column, Summary, TotalItem, Pager, Paging } from "devextreme-react/data-grid";
 import "devextreme/dist/css/dx.light.css";
 import "./Grid.css";
 import { useState } from "react";
 import Button from "../Button/Button"
+import { FiSearch } from 'react-icons/fi';
+import { HiFilter } from 'react-icons/hi';
+
 
 function Grid() {
     const [searchText, setSearchText] = useState('');
+    const [data, setData] = useState([]);
+    const [pageSize, setPageSize] = useState(10); // varsayılan satır sayısı
+
+
+    // Verileri filtrelemek için bir işlev
+  const filteredData = data.filter(item => {
+    // İlgili alanlarda (link, name, description) arama yap
+    const linkMatch = item.link.toLowerCase().includes(searchText.toLowerCase());
+    const nameMatch = item.name.toLowerCase().includes(searchText.toLowerCase());
+    const descriptionMatch = item.description.toLowerCase().includes(searchText.toLowerCase());
+    // Herhangi bir alanda eşleşme varsa, bu öğeyi filtrelenmiş veriye dahil et
+    return linkMatch || nameMatch || descriptionMatch;
+  });
+
 
   return (
     <div className="datagrid">
-         <div className="container">
-        <div>
+    <div className="container">
+
+
+      <div className="input-container">
+        <div className="input-section">
           <input 
             type="text"
-            placeholder="Arama..."
+            placeholder="Search objects..."
             value={searchText}
             onChange={e => setSearchText(e.target.value)}
           />
-          <button>Filtrele</button>
         </div>
-          <Button/>
-
+        <div className="icon-section">
+          <FiSearch className="search-icon" />
+        </div>
+        <button>
+          <HiFilter className="icon"/>
+        </button>
       </div>
-      <DataGrid showBorders={true} rowAlternationEnabled={true}>
-        <Column dataField="link" caption="Sosyal Medya Linki" />
-        <Column dataField="name" caption="Sosyal Medya Adı" />
-        <Column dataField="description" caption="Açıklama" />
-      </DataGrid>
+      <Button setData={setData}/>
     </div>
+    <DataGrid dataSource={filteredData} showBorders={true} rowAlternationEnabled={true}>
+      <Column dataField="link" caption="Sosyal Medya Linki" />
+      <Column dataField="name" caption="Sosyal Medya Adı" />
+      <Column dataField="description" caption="Açıklama" />
+      <label>Rows: </label>
+    <input 
+          type="number"
+          min="1"
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        />
+     <Summary>
+          <TotalItem 
+            column="link"
+            summaryType="count"
+            displayFormat={'{0} rows'}
+          />
+        </Summary>
+        <Paging defaultPageSize={pageSize} />
+        <Pager allowedPageSizes={[5, 10, 20]} showPageSizeSelector={true} />
+    </DataGrid>
+  </div>
   );
 }
 
